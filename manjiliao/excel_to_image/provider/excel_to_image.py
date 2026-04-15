52dfeee0b36f1b990df713797cc035c62f6775cc
LIUZM <liuzm@kinnit.com>
2026-04-15 10:01:52 +0800

    Remove source code files, keep only .difypkg package

--- manjiliao/excel_to_image/provider/excel_to_image.py
+++
@@ -1,8 +0,0 @@
-from typing import Any
-
-from dify_plugin import ToolProvider
-
-
-class FreeSpireExcelToImageProvider(ToolProvider):
-    def _validate_credentials(self, credentials: dict[str, Any]) -> None:
-        return None
